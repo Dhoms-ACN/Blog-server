82c9ad57b809392d2091d127043decfcc552d8e7
Pelago <domingo.pelago@accenture.com>
2022-03-29 12:26:38 +0800

    Initial commit

---
+++ database/mongoose.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+mongoose.connect(process.env.DB_URI).then(() => console.log('MongoDB Connected!'))
---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+require('./database/mongoose')
+const app = express()
+const port = process.env.PORT || 3001
+
+
+app.listen(port, () => console.log(`App running on port ${port}`));
